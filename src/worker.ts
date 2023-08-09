11fe4dfde1e5e0ffd3aea56720ecf752635c131c
Ankur Datta <datta@prisma.io>
2023-08-09 15:34:35 +0600

    remove node_modules

--- src/worker.ts
+++ src/worker.ts
@@ -8,25 +8,43 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	DATABASE_URL: string;
 }
+
+let client: PrismaClient;
+
+const prisma = (env: Env) => {
+	if (client) {
+		return client;
+	}
+
+	client = new PrismaClient({
+		datasources: {
+			db: {
+				url: env.DATABASE_URL,
+			},
+		},
+	});
+
+	return client;
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response(
+			await prisma(env)
+				.$extends(withAccelerate())
+				.notes.findMany({
+					take: 20,
+					cacheStrategy: {
+						ttl: 3600,
+					},
+				})
+				.then((data) => JSON.stringify(data))
+		);
 	},
 };
